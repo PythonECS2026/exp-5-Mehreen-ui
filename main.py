1371154dfa6abeacef61ebbea284b2851df471a3
Mehreen <mehreenansari4954@gmail.com>
2026-02-20 17:57:31 +0530

    Enhance task management features in main.py
    Added functionality to manage tasks including adding, editing, removing, and sorting tasks.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # AIM: Task List Manager
-# Coder:
-# Date:
+# Coder: mehreen ansari
+# Date: 20/02/2026
 
 print("--- Task List Manager ---")
 tasks = ["Sleep", "Getup", "Brush"]
 print(f"Original Tasks: {tasks}")
 
-# Write your code here
-# TODO: Add & Print new Task from user
+tasks.append(input())
+print(f"Tasks after Adding: {tasks}")
+edit_index = int(input())
+tasks[edit_index] = input()
+print(f"Tasks after Editing: {tasks}")
+tasks.pop(0)
+print(f"Tasks after Removing: {tasks}")
+tasks.sort()
+print(f"Tasks after Sorting: {tasks}") 
 
-# TODO: Edit & Print task selected by User
 
-# TODO: Remove & Print a Task selected by User
-
-# TODO: Sort & Print the Tasks
